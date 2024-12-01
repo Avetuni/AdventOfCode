8b25896ad6bff1ebb46c83b62199ab388b0f8f6b
Avetuni <135461387+Avetuni@users.noreply.github.com>
2024-12-01 07:00:11 +0100

    Py solution 01

--- 2024/01/part1.py
+++ 2024/01/part1.py
@@ -1,2 +1,21 @@
-with open("input.txt", "r") as file:
-    while line := file.readline():
+
+sum = 0
+arr1 = []
+arr2 = []
+
+with open("2024\\01\\input.txt", "r") as file:
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        split_line = line.strip().split("   ")
+        if len(split_line) >= 2:
+            arr1.append(int(split_line[0]))
+            arr2.append(int(split_line[1]))
+arr1.sort()
+arr2.sort()
+
+for i in range(len(arr1)):
+    sum += abs(arr1[i] - arr2[i])
+
+print(sum)
--- 2024/01/part2.py
+++ 2024/01/part2.py
@@ -1,2 +1,23 @@
-with open("input.txt", "r") as file:
-    while line := file.readline():
+sum = 0
+arr1 = []
+arr2 = []
+
+with open("2024\\01\\input.txt", "r") as file:
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        split_line = line.strip().split("   ")
+        if len(split_line) >= 2:
+            arr1.append(int(split_line[0]))
+            arr2.append(int(split_line[1]))
+arr1.sort()
+arr2.sort()
+
+for i in arr1:
+    numToCheck = i
+    for j in arr2:
+        if j == i:
+            sum += i
+
+print(sum)
