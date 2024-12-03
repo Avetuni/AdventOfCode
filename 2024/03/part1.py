ee7d0af6d6bb598dde18d2d80d02466f1f99cecb
Avetuni <135461387+Avetuni@users.noreply.github.com>
2024-12-03 10:12:43 +0100

    3.1 solution

--- 2024/03/part1.py
+++ 2024/03/part1.py
@@ -1,2 +1,16 @@
+import re
+
+sum = 0
+matches = []
+
 with open("input.txt", "r") as file:
-    while line := file.readline():
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        matches = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", line)
+        for i in matches:
+            sum += int(i[0]) * int(i[1])
+
+
+print(sum)
