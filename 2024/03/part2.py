449495b70147bfc5e46b31430ad9124d5605ce20
Avetuni <135461387+Avetuni@users.noreply.github.com>
2024-12-03 10:13:02 +0100

    3.2 solution

--- 2024/03/part2.py
+++ 2024/03/part2.py
@@ -1,2 +1,23 @@
+import re
+
+sum = 0
+matches = []
+curInstruction = True
+
 with open("input.txt", "r") as file:
-    while line := file.readline():
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        matches = re.findall(r"(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))", line)
+        for i in matches:
+            if i[0]:
+                if curInstruction:
+                    sum += int(i[1]) * int(i[2])
+            elif i[3]:
+                curInstruction = True
+            elif i[4]:
+                curInstruction = False
+
+
+print(sum)
