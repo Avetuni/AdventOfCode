689b46f69b46b0d3ea104e28b4eb3dde8ce3cf02
Avetuni <135461387+Avetuni@users.noreply.github.com>
2025-02-25 12:10:07 +0100

    Update part1.py
    why tf is the result over 6 trillion

--- 2024/06/part1.py
+++ 2024/06/part1.py
@@ -1,2 +1,29 @@
+import itertools
+result = 0
+def permutations(length):
+    return [''.join(p) for p in itertools.product(["+", "*"], repeat=length)]
+
 with open("input.txt", "r") as file:
-    while line := file.readline():
+    while line := file.readline().strip():
+        if ":" not in line:
+            break
+        line = line.split(":")
+        line[1] = line[1].strip().split(" ")
+        perms = permutations(len(line[1]))
+        for i in perms:
+            operators = list(i)
+            res = 0
+            for j in range(len(line[1])):
+                if j != 0:
+                    if operators[j-1] == "+":
+                        res += int(line[1][j])
+                    else:
+                        res *= int(line[1][j])
+                else:
+                    res = int(line[1][j])
+            if res == int(line[0]):
+                result += int(line[0])
+                break
+                
+                
+print(result)
