b638c95004e3527daa720fe69f9bddcb992e918c
Avetuni <135461387+Avetuni@users.noreply.github.com>
2025-02-25 12:17:00 +0100

    Unfinished prt 2

--- 2024/06/part2.py
+++ 2024/06/part2.py
@@ -1,2 +1,32 @@
-with open("input.txt", "r") as file:
-    while line := file.readline():
+import itertools
+result = 0
+def permutations(length):
+    return [''.join(p) for p in itertools.product(["+", "*", "||"], repeat=length)]
+
+with open("test.txt", "r") as file:
+    while line := file.readline().strip():
+        if ":" not in line:
+            break
+        line = line.split(":")
+        line[1] = line[1].strip().split(" ")
+        perms = permutations(len(line[1]))
+        for i in perms:
+            operators = list(i)
+            res = 0
+            for j in range(len(line[1])):
+                if j != 0:
+                    if operators[j-1] == "+":
+                        res += int(line[1][j])
+                    elif operators[j-1] == "*":
+                        res *= int(line[1][j])
+                    else:
+                        res = int(str(res)+line[1][j])
+                        print(str(int(str(res)+line[1][j])), str(res), line[1][j])
+                else:
+                    res = int(line[1][j])
+            if res == int(line[0]):
+                result += int(line[0])
+                break
+                
+                
+print(result)
