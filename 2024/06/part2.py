6a9cd8755b9dab7ce923b5142ff5d4c8eb0162bd
Avetuni <135461387+Avetuni@users.noreply.github.com>
2025-03-11 10:00:46 +0100

    Finish part2.py

--- 2024/06/part2.py
+++ 2024/06/part2.py
@@ -1,9 +1,9 @@
 import itertools
 result = 0
 def permutations(length):
-    return [''.join(p) for p in itertools.product(["+", "*", "||"], repeat=length)]
+    return [''.join(p) for p in itertools.product(["+", "*", "|"], repeat=length)]
 
-with open("test.txt", "r") as file:
+with open("input.txt", "r") as file:
     while line := file.readline().strip():
         if ":" not in line:
             break
@@ -21,11 +21,11 @@ with open("test.txt", "r") as file:
                         res *= int(line[1][j])
                     else:
                         res = int(str(res)+line[1][j])
-                        print(str(int(str(res)+line[1][j])), str(res), line[1][j])
                 else:
                     res = int(line[1][j])
             if res == int(line[0]):
                 result += int(line[0])
+                print(int(line[0]))
                 break
                 
                 
